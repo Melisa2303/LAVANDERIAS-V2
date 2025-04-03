87c052e7c8b5e11dc554db195a43c952d649b443
Melisa2303 <melisa.mezadelg@gmail.com>
2025-04-02 22:15:38 -0500

    Update upload_csv_to_firestore.py

--- upload_csv_to_firestore.py
+++ upload_csv_to_firestore.py
@@ -8,7 +8,7 @@ cred = credentials.Certificate({
     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"],
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
