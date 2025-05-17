142bc1aa824e42802140e3804d575fc18d83e383
Melisa2303 <melisa.mezadelg@gmail.com>
2025-05-17 18:27:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def verificar_unicidad_boleta(numero_boleta, tipo_servicio, sucursal):
 def login():
     col1, col2 = st.columns([1, 3])
     with col1:
-        st.image("https://github.com/Melisa2303/LAVANDERIAS-V2/raw/main/LOGO.PNG", width=100)
+        st.image("https://github.com/Melisa2303/LAVANDERIAS-V2/raw/main/data/LOGO.PNG", width=100)
     with col2:
         st.markdown("<h1 style='text-align: left; color: black;'>Lavanderías Americanas</h1>", unsafe_allow_html=True)
     
@@ -132,7 +132,7 @@ def login():
 def ingresar_boleta():
     col1, col2 = st.columns([1, 3])
     with col1:
-        st.image("data/LOGO.png", width=100)
+        st.image("https://github.com/Melisa2303/LAVANDERIAS-V2/raw/main/data/LOGO.PNG", width=100)
     with col2:
         st.markdown("<h1 style='text-align: left; color: black;'>Lavanderías Americanas</h1>", unsafe_allow_html=True)
     st.title("📝 Ingresar Boleta")
