fc297af81ee69a8e33a931b65de2819a7c795e7e
Melisa2303 <melisa.mezadelg@gmail.com>
2025-05-17 18:25:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def login():
 def ingresar_boleta():
     col1, col2 = st.columns([1, 3])
     with col1:
-        st.image("https://github.com/Melisa2303/LAVANDERIAS-V2/raw/main/LOGO.PNG", width=100)
+        st.image("data/LOGO.png", width=100)
     with col2:
         st.markdown("<h1 style='text-align: left; color: black;'>Lavanderías Americanas</h1>", unsafe_allow_html=True)
     st.title("📝 Ingresar Boleta")
