fe15f610f6caed9f833fd6f360fc1b949266701f
Melisa2303 <melisa.mezadelg@gmail.com>
2025-05-31 10:41:52 -0500

    Update rutas.py

--- features/rutas.py
+++ features/rutas.py
@@ -9,7 +9,7 @@ from googlemaps.convert import decode_polyline
 from streamlit_folium import st_folium
 import folium
 from datetime import datetime, timedelta, time
-from algorithms.algoritmo1 import optimizar_ruta_algoritmo1, cargar_pedidos, _crear_data_model, _distancia_duracion_matrix, 
+from algorithms.algoritmo1 import optimizar_ruta_algoritmo1, cargar_pedidos, _crear_data_model, _distancia_duracion_matrix
 from algorithms.algoritmo2 import optimizar_ruta_algoritmo2
 from algorithms.algoritmo3 import optimizar_ruta_algoritmo3
 from algorithms.algoritmo4 import optimizar_ruta_algoritmo4
