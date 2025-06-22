778ab294fc039e21a3bcbea1b752787db270f5de
KashMir-dot <angie.miranda@ucsp.edu.pe>
2025-06-22 18:03:45 -0500

    Update algoritmo22.py

--- algorithms/algoritmo22.py
+++ algorithms/algoritmo22.py
@@ -348,7 +348,7 @@ def _haversine_meters(lat1, lon1, lat2, lon2):
     a = math.sin(dphi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
     return 2 * R * math.asin(math.sqrt(a))
 
-def agrupar_puntos_aglomerativo(df, eps_metros=300):
+def agrupar_puntos_aglomerativo(df, eps_metros=20):
     """
     Agrupa pedidos cercanos mediante AgglomerativeClustering. 
     eps_metros: umbral de distancia máxima en metros para que queden en el mismo cluster.
