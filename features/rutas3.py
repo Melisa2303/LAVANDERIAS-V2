de72cea8023504a2f460171a5112cf2f63b5c638
FrancoCH25 <franco.choque@ucsp.edu.pe>
2025-06-27 09:25:48 -0500

    Update rutas3.py

--- features/rutas3.py
+++ features/rutas3.py
@@ -19,7 +19,7 @@ from core.constants import GOOGLE_MAPS_API_KEY
 
 from algorithms.algoritmo1 import optimizar_ruta_algoritmo22, cargar_pedidos, _crear_data_model, agrupar_puntos_aglomerativo,MARGEN, SHIFT_START_SEC, SHIFT_END_SEC
 from algorithms.algoritmo2 import optimizar_ruta_cw_tabu
-from algorithms.algoritmo3 import optimizar_ruta_cp_sat, crear_data_model_cp, corregir_ventanas_iguales
+from algorithms.algoritmo3 import optimizar_ruta_cp_sat
 from algorithms.algoritmo4 import optimizar_ruta_algoritmo4
 
 # Coordenadas fijas de la cochera
