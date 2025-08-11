16f8c4830fa2fd2453320a0d46306737bb35cd04
KashMir-dot <angie.miranda@ucsp.edu.pe>
2025-08-10 23:34:34 -0500

    Update algoritmo1.py

--- algorithms/algoritmo1.py
+++ algorithms/algoritmo1.py
@@ -43,8 +43,8 @@ gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
 # -------------------- CONSTANTES VRP --------------------
 SERVICE_TIME    = 10 * 60        # 10 minutos de servicio en cada parada (excepto depósito)
 MAX_ELEMENTS    = 100            # límite de celdas por petición Distance Matrix API
-SHIFT_START_SEC =  9 * 3600      # 09:00 en segundos
-SHIFT_END_SEC   = 16*3600 +30*60 # 16:30 en segundos
+SHIFT_START_SEC =  8.5 * 3600      # 09:00 en segundos
+SHIFT_END_SEC   = 17*3600 # 16:30 en segundos
 MARGEN = 15 * 60  # 15 minutos en segundos
 
 # ===================== FUNCIONES AUXILIARES =====================
