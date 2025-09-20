069a51b6ba2a6fd6eb3fec7ae526c59a3f25fa96
Melisa2303 <melisa.mezadelg@gmail.com>
2025-09-20 16:49:38 -0500

    Update tracking.py

--- features/tracking.py
+++ features/tracking.py
@@ -49,7 +49,7 @@ def seguimiento_vehiculo():
     # Encabezado
     col1, col2 = st.columns([1, 3])
     with col1:
-        st.image("https://github.com/Melisa2303/LAVANDERIAS-V2/raw/main/LOGO.PNG", width=100)
+        st.image("https://github.com/Melisa2303/LAVANDERIAS-V2/raw/main/data/LOGO.PNG", width=100)
     with col2:
         st.markdown("<h1 style='text-align: left; color: black;'>Lavanderías Americanas</h1>", unsafe_allow_html=True)
     st.title("📍 Seguimiento de Vehículo")
