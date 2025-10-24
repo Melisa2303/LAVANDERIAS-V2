f5908b330f1dbc70bfe2c2b2542e07f9b0fd5e0b
Melisa2303 <melisa.mezadelg@gmail.com>
2025-10-23 19:22:33 -0500

    Update tracking.py

--- features/tracking.py
+++ features/tracking.py
@@ -7,10 +7,10 @@ from streamlit_folium import st_folium
 # ===========================
 # CONFIGURACIÓN
 # ===========================
-TRACCAR_URL = "https://TU-URL-DE-TRACCAR.onrender.com"  # Cambia por tu URL real
-USERNAME = "admin"     # Cambia si usas otro usuario
-PASSWORD = "admin"     # Cambia por tu contraseña
-DEVICE_ID = 6          # Cambia por tu ID real
+TRACCAR_URL = "https://traccar-production-8d92.up.railway.app/"  # Cambia por tu URL real
+USERNAME = "Lavanderías Americanas"     # Cambia si usas otro usuario
+PASSWORD = "lav123"     # Cambia por tu contraseña
+DEVICE_ID = Arianne          # Cambia por tu ID real
 
 # ===========================
 # FUNCIONES
